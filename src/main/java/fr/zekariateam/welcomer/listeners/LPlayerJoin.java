27215ade9b1ce7f4f1bb1356ac8482c0d02e477f
Alexis <velristv@ynov.com>
2023-01-19 14:06:25 +0100

    :zap: LPlayerJoin added

--- src/main/java/fr/zekariateam/welcomer/listeners/LPlayerJoin.java
+++ src/main/java/fr/zekariateam/welcomer/listeners/LPlayerJoin.java
@@ -1,6 +1,16 @@
 package fr.zekariateam.welcomer.listeners;
 
+import fr.zekariateam.welcomer.Welcomer;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class LPlayerJoin implements Listener {
+
+    private final Welcomer main = Welcomer.getInstance();
+
+    public void OnPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+    }
+
 }
